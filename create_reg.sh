4c23ab20e5d13a49f2387881837fbf357ee41788
Markus Metzmacher <info@3dgrid.de>
2016-03-26 14:48:36 +0100

    implemented parameter

--- create_reg.sh
+++ create_reg.sh
@@ -2,3 +2,12 @@
 
 # Script to create regions
 # more descriptions here
+
+source config/reg.cfg
+
+echo "Path for OpenSim base directory: $OpenSim_Base" 
+echo "Path for OpenSim Land directory: $OpenSim_Land"
+echo "Path for OpenSim OAR directory: $OpenSim_Oar"
+echo "Path for OpenSim PID directory: $OpenSim_Pid"
+echo "Path for OpenSim LOG directory: $OpenSim_Log"
+echo "Path for OpenSim DB directory: $OpenSim_Db"
