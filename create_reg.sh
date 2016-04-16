7f7214f9614d6be9dc45b52c00f4449afea0e2ca
Markus Metzmacher <info@3dgrid.de>
2016-04-16 16:53:26 +0200

    Creation of files added!

--- create_reg.sh
+++ create_reg.sh
@@ -8,6 +8,7 @@ UUID=$(uuidgen)
 RegionName=$2
 RegionNameFileSys=${RegionName,,}
 Port=$3
+Coordinates=$4
 
 clear
 
@@ -32,41 +33,114 @@ echo "Directory to be created in $OpenSim_Land: $RegionNameFileSys"
 echo "Region in Simulation will be named: $RegionName"
 echo ""
 
-read -p "Übernehmen (j/n)? " response
+		case $1 in
 
-if [ "$response" == "j" ]; then
+			list)
+				ls -al $OpenSim_Land
+				echo "NOTE: it will be tested whether a Region exists or not"
+			;;
 
-        if [ -d $OpenSim_Land/$RegionNameFileSys ] ; then
-                echo ""
-                echo "Directory existing - will abort the operation"
-		echo ""
-                echo "PLEASE CHOOSE A REGION NAME, WICH DOES NOR EXIST ON THIS SERVER"
-		echo ""
-		exit
-        else
-                echo""
-		echo "$OpenSim_Land/$RegionNameFileSys will be created in $OpenSim_Land"
-		echo "Region will be found by searching $RegionName in Opensimulatior Viewer"
-		echo""
-	
-case $1 in
+			create)
+				read -p "Übernehmen (j/n)? " response
 
-	list)
-	ls -al $OpenSim_Land
-	echo "NOTE: it will be tested whether a Region exists or not"
-	;;
+				if [ "$response" == "j" ]; then
 
-	create)
-	mkdir $OpenSim_Land/$RegionNameFileSys
-	;;
+        			if [ -d $OpenSim_Land/$RegionNameFileSys ] ; then
+                		echo ""
+                		echo "Directory existing - will abort the operation"
+                        echo ""
+                		echo "PLEASE CHOOSE A REGION NAME, WICH DOES NOR EXIST ON THIS SERVER"
+                        echo ""
+                		exit
+        			else
+                		echo""
+                        echo "$OpenSim_Land/$RegionNameFileSys will be created in $OpenSim_Land"
+                        echo "Region will be found by searching $RegionName in Opensimulatior Viewer"
+                        echo""
+						mkdir $OpenSim_Land/$RegionNameFileSys
+						mkdir  $OpenSim_Land/$RegionNameFileSys/Regions
+						touch  $OpenSim_Land/$RegionNameFileSys/OpenSim.ini
+						echo "[Startup]
+PIDFile = $OpenSim_Pid
+regionload_regionsdir =  $OpenSim_Land/$RegionNameFileSys/Regions/
+Stats_URI = "jsonSimStats"
 
-	delete)
-	;;
+[Network]
+http_listener_port = $3" >> $OpenSim_Land/$RegionNameFileSys/OpenSim.ini
+						touch  $OpenSim_Land/$RegionNameFileSys/OpenSim.exe.config
+						echo "<?xml version="1.0" encoding="utf-8" ?>
+<configuration>
+  <configSections>
+    <section name="log4net" type="log4net.Config.Log4NetConfigurationSectionHandler,log4net" />
+  </configSections>
+  <runtime>
+    <gcConcurrent enabled="true" />
+        <gcServer enabled="true" />
+  </runtime>
+  <appSettings>
+  </appSettings>
+  <log4net>
+    <appender name="Console" type="OpenSim.Framework.Console.OpenSimAppender, OpenSim.Framework.Console">
+      <layout type="log4net.Layout.PatternLayout">
+        <conversionPattern value="%date{HH:mm:ss} - %message" />
+        <!-- console log with milliseconds.  Useful for debugging -->
+<!--        <conversionPattern value="%date{HH:mm:ss.fff} - %message" /> -->
+      </layout>
+    </appender>
 
-	config)
-	;;
+<appender name="RollingFileAppender" type="log4net.Appender.RollingFileAppender">
+  <file value=$OpenSim_Log/$RegionName.log />
+  <appendToFile value="true" />
+  <maximumFileSize value="1000KB" />
+  <maxSizeRollBackups value="2" />
+  <layout type="log4net.Layout.PatternLayout">
+    <conversionPattern value="%date %-5level - %logger %message%newline" />
+  </layout>
+</appender>
 
-*)
-esac
-fi
-fi
+    <root>
+      <level value="DEBUG" />
+      <appender-ref ref="Console" />
+      <appender-ref ref="RollingFileAppender" />
+    </root>
+
+    <!-- Independently control logging level for XEngine -->
+    <logger name="OpenSim.Region.ScriptEngine.XEngine">
+      <level value="INFO"/>
+    </logger>
+
+    <!-- Independently control logging level for per region module loading -->
+    <logger name="OpenSim.ApplicationPlugins.RegionModulesController.RegionModulesControllerPlugin">
+      <level value="INFO"/>
+    </logger>
+
+  </log4net>
+</configuration>" >> $OpenSim_Land/$RegionNameFileSys/OpenSim.exe.config
+						touch  $OpenSim_Land/$RegionNameFileSys/Regions/Regions.ini
+						echo "[$RehionName]
+RegionUUID = $UUID
+Location = $4
+InternalAddress = 0.0.0.0
+InternalPort = $3
+AllowAlternatePorts = False
+ExternalHostName = SYSTEMIP
+RegionType = "Mainland"
+MaxPrims = 45000
+MaxAgents = 40" >> $OpenSim_Land/$RegionNameFileSys/Regions/Regions.ini
+
+					fi
+				else
+					echo "ABORTING"
+					exit
+				fi
+			;;
+
+			delete)
+			;;
+
+			config)
+			;;
+
+			*)
+		esac
+
